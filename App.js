2872cd021af98bfa6fc041fac956cbc52ba54031
Piyush <dntrplytch@gmail.com>
2024-08-30 16:25:00 +0530

    "dummy" version of the Shwaas Android app in expo

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const now = new Date();
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Shwass_{now.getHours()}_{now.getMinutes()} - Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
